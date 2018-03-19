530fbbb2818195407d757dee79d0a0aa461b778c
falsuwailih <fahad.a.alsuwailih@gmail.com>
2018-03-19 18:24:15 +0300

    Prepare the login page

--- App.js
+++ App.js
@@ -1,21 +1,19 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-
+import { StackNavigator } from "react-navigation";
+import Login from "./app/components/Login.js";
+const Application = StackNavigator(
+  {
+    Home: {
+      screen: Login
+    }
+  },
+  {
+    navigationOptions: { header: false }
+  }
+);
 export default class App extends React.Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Hello Fahad From Windows 10</Text>
-      </View>
-    );
+    return <Application />;
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
